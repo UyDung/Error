962d0ebf955ad8ab57aa8d17e3e90656a106fcca
UyDung <86514728+UyDung@users.noreply.github.com>
2021-09-25 21:47:51 +0700

    Create useContext-with-useReducer.js

---
+++ useContext-with-useReducer.js
@@ -0,0 +1,79 @@
+import { useReducer } from "react";
+
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+    totalPrice: 0,
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === "ADD") {
+        const updatedTotalPrice = state.totalPrice + action.item.amount * action.item.price;
+        const updatedTotalAmount = state.totalAmount + action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex((item) => (
+            item.id === action.item.id
+        ));
+
+        const existingItem = state.items[existingCartItemIndex];
+
+        let updatedItems;
+        if (existingCartItemIndex >= 0) {
+            
+            const updatedItem = {...existingItem, amount: existingItem.amount + action.item.amount };
+  
+            updatedItems = [...state.items]; 
+          //   updatedItems = state.items;  // De the nay cung chay ok
+          //   updatedItems = [state.items]; // De nhu nay chay qua 1 vai lan click add item la bao loi TypeError: Cannot read properties of undefined (reading 'id')
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+            totalPrice: updatedTotalPrice,
+        };
+    }
+
+    if (action.type === "REMOVE") {
+        const updatedTotalPrice = state.totalPrice - action.item.amount * action.item.price;
+        const updatedTotalAmount = state.totalAmount + action.item.amount;
+
+        const updatedItems = state.items.concat(action.item);
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+            totalPrice: updatedTotalPrice,
+        };
+    }
+
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCart = (item) => {
+        dispatchCartAction({ type: "ADD", item: item });
+    };
+
+    const removeItemFromCart = (id) => {};
+
+    const cartContext = {
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        totalPrice: cartState.totalPrice,
+        addItem: addItemToCart,
+        removeItem: removeItemFromCart,
+    };
+
+    return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+};
+
+export default CartProvider;
